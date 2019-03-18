5c715abc9cae200de6f68eeb3df8937f0993a477
Aaron Ault <ault@purdue.edu>
2019-03-18 17:16:39 -0400

    Updates

---
+++ precache-manifest.4fa1849a86cc20486f6b1cf9ac5b6b48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0325473559e62adc9167",
+    "url": "/account-paster/static/css/main.2f4c85b2.chunk.css"
+  },
+  {
+    "revision": "0325473559e62adc9167",
+    "url": "/account-paster/static/js/main.c0d12372.chunk.js"
+  },
+  {
+    "revision": "2bb437e911b97203ebe4",
+    "url": "/account-paster/static/js/runtime~main.e31fc3a0.js"
+  },
+  {
+    "revision": "76c65f0115217480ddb6",
+    "url": "/account-paster/static/js/2.d09e5813.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/account-paster/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3ab877ea29db29a385cb4e9e70e1f683",
+    "url": "/account-paster/index.html"
+  }
+];
